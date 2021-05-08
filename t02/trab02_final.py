4931283a5f5d26e67f1708acb62c9bc5db61305a
Eduardo Siqueira <eduardo.sousa.siqueira@gmail.com>
2021-05-08 18:11:34 -0300

    comentarios finais

--- t02/trab02_final.py
+++ t02/trab02_final.py
@@ -58,28 +58,32 @@ def gamma_correction(images, gamma):
 
     return np.asarray(new_images)
 
-#realiza a superresolucao
+#realiza a superresolucao generalizada para imagens de qualquer dimensao
+#essa funcao exige um numero de imagens que possui raiz quadrada inteira
 def superresolution(images):
     #assumindo resolução segundo proposta do trabalho
     n_images = images.shape[0]  #qtde de imagens
     h_res = (images.shape[1]*math.sqrt(n_images), images.shape[2]*math.sqrt(n_images)) #resolucao da imagem final
-    h_image = np.empty(np.int32(h_res))
-    step = np.int32(math.sqrt(n_images))
-    x_offset = 0
-    y_offset = 0
-    id_imgs = 0
+    h_image = np.empty(np.int32(h_res))     #criando uma matriz vazia com as dimensoes obtidas
+    step = np.int32(math.sqrt(n_images))    #passo que sera usado para calculo da coordenada na imagen final
+    id_imgs = 0     #variavel auxiliar para o preenchimento da imagem final
 
     #construindo a cena a partir das sub-imagens
     for img in images:
         n_lines, n_cols = img.shape
+        #para calculo das linhas, é feita a divisao baseado no indice da imagem sendo processada pois o preenchimento é feito em ordem de colunas
+        x_offset = (id_imgs) // step #divisao inteira
+        #para calculo das colunas, pegamos o resto da divisao inteira acima
+        y_offset = (id_imgs) % step
+        #para cada imagem de baixa resoluçao, colocamos seus pixels na imagem final
         for i in np.arange(n_lines):
             for j in np.arange(n_cols):
+                #calculo dos indices da imagem de alta resolucao
+                #step determina o quadrante, e os offsets encontram o superpixel certo baseado na imagem de baixa res
                 x,y = (i*step + x_offset, j*step + y_offset)
                 h_image[x][y] = img[i][j]
 
         id_imgs += 1
-        y_offset = (id_imgs) % step
-        x_offset = (id_imgs) // step #divisao inteira
 
     return h_image
 

582d6d677654f118be9f038940fa8cf99128d587
Eduardo Siqueira <eduardo.sousa.siqueira@gmail.com>
2021-05-15 15:09:37 -0300

    .

---
+++ t03/trab03.py
@@ -0,0 +1,16 @@
+# Nomes:    Eduardo de Sousa Siqueira       nUSP: 9278299
+#           Igor Barbosa Grécia Lúcio       nUSP: 9778821
+# SCC0251 - Image Processing          1 semestre 2021
+# Trabalho 03. Filtragem
+
+import numpy as np
+import imageio as io
+
+
+
+#Começo do programa principal
+
+#Lendo entrada do programa
+imgref = input().rstrip()
+F = input().rstrip()
+
